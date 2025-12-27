e9d7ab6c3bc5ba8664ae00b3f243941a9a27ef60
Shraddha Desai <shradsd104@gmail.com>
2025-12-27 22:31:55 +0530

    Small fix: importing issue

--- dashboard/src/components/Menu.js
+++ dashboard/src/components/Menu.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { useCookies } from "react-cookie";
+import axios from "axios";
 
 const Menu = ({username}) => {
   const [selectedMenu, setSelectedMenu] = useState(0);
